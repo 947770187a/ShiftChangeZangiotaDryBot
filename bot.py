1f080172dde3ae020f6ead6456bcddd3761f4aa3
947770187a <947770187a@gmail.com>
2026-07-15 21:47:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
@@ -14,6 +14,14 @@ async def start(message: Message):
     await message.answer(
         "✅ Shift Change Bot запущен.\n\n"
         "Добро пожаловать!"
+    )
+
+
+async def send_text(chat_id: int, text: str):
+
+    await bot.send_message(
+        chat_id=chat_id,
+        text=text
     )
 
 
