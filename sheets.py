4c2da681e5b014741c9b042fb84b5a4b7e3abc6f
947770187a <947770187a@gmail.com>
2026-07-15 18:28:25 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -31,6 +31,8 @@ class GoogleSheets:
         if os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"):
 
             data = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
+            print("ENV EXISTS:", "GOOGLE_SERVICE_ACCOUNT_JSON" in os.environ)
+print("ENV LENGTH:", len(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]))
 
             creds = Credentials.from_service_account_info(
                 data,
