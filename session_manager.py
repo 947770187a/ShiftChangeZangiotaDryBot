0253ca6d80d7b4bbc6dcaa064f12c59bc4aa85f8
947770187a <947770187a@gmail.com>
2026-07-15 21:29:22 +0500

    Update session_manager.py

--- session_manager.py
+++ session_manager.py
@@ -1,1 +1,31 @@
+import uuid
+from datetime import datetime
 
+
+class SessionManager:
+
+    def __init__(self, sheets):
+
+        self.sheets = sheets
+
+    def create_session(self, schedule):
+
+        session = {
+            "SessionID": str(uuid.uuid4()),
+            "ScheduleID": schedule["ScheduleID"],
+            "StartDateTime": datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
+            "SenderUserID": schedule["SenderUserID"],
+            "ReceiverUserID": "",
+            "Status": "CREATED",
+            "AcceptDateTime": "",
+            "FinishDateTime": ""
+        }
+
+        print()
+        print("=" * 50)
+        print("SESSION CREATED")
+        print(session)
+        print("=" * 50)
+        print()
+
+        return session
