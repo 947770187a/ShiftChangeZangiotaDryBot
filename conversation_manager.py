82d7dcb78a72256b4f7dabd89e39ffd33f93f4a9
947770187a <947770187a@gmail.com>
2026-07-15 22:21:22 +0500

    Update conversation_manager.py

--- conversation_manager.py
+++ conversation_manager.py
@@ -8,15 +8,43 @@ class ConversationManager:
         self.sheets = sheets
         self.state_manager = StateManager(sheets)
 
-    async def process_message(
+    async def process_telegram_message(
         self,
-        session,
-        user,
-        message
+        telegram_id,
+        text
     ):
+
+        user = self.find_user_by_telegram(telegram_id)
+
+        if user is None:
+            return
+
+        session = self.sheets.get_session_by_sender(
+            user["UserID"]
+        )
+
+        if session is None:
+
+            session = self.sheets.get_session_by_receiver(
+                user["UserID"]
+            )
+
+        if session is None:
+            return
 
         await self.state_manager.process_message(
             session=session,
             user=user,
-            message=message
+            message=text
         )
+
+    def find_user_by_telegram(self, telegram_id):
+
+        users = self.sheets.get_users()
+
+        for user in users:
+
+            if str(user["TelegramID"]) == str(telegram_id):
+                return user
+
+        return None
