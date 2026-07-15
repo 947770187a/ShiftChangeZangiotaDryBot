4911f36f2b9154efd683d87a7921f07b3c5b6411
947770187a <947770187a@gmail.com>
2026-07-15 22:16:41 +0500

    Update conversation_manager.py

--- conversation_manager.py
+++ conversation_manager.py
@@ -1,1 +1,22 @@
+from state_manager import StateManager
 
+
+class ConversationManager:
+
+    def __init__(self, sheets):
+
+        self.sheets = sheets
+        self.state_manager = StateManager(sheets)
+
+    async def process_message(
+        self,
+        session,
+        user,
+        message
+    ):
+
+        await self.state_manager.process_message(
+            session=session,
+            user=user,
+            message=message
+        )
