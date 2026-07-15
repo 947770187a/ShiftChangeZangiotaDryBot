d9ba4651cc23781deb1012ed249618554aba4b7b
947770187a <947770187a@gmail.com>
2026-07-15 22:15:25 +0500

    Update state_manager.py

--- state_manager.py
+++ state_manager.py
@@ -1,1 +1,73 @@
+class StateManager:
 
+    def __init__(self, sheets):
+
+        self.sheets = sheets
+
+    async def process_message(
+        self,
+        session,
+        user,
+        message
+    ):
+
+        status = session["Status"]
+
+        if status == "WAITING_SENDER_ANSWER":
+
+            await self.process_sender_answer(
+                session,
+                user,
+                message
+            )
+
+            return
+
+        if status == "WAITING_RECEIVER_CONFIRM":
+
+            await self.process_receiver_confirm(
+                session,
+                user,
+                message
+            )
+
+            return
+
+        if status == "WAITING_RECEIVER_ANSWER":
+
+            await self.process_receiver_answer(
+                session,
+                user,
+                message
+            )
+
+            return
+
+        print(f"Unknown status: {status}")
+
+    async def process_sender_answer(
+        self,
+        session,
+        user,
+        message
+    ):
+
+        print("Sender answer")
+
+    async def process_receiver_confirm(
+        self,
+        session,
+        user,
+        message
+    ):
+
+        print("Receiver confirm")
+
+    async def process_receiver_answer(
+        self,
+        session,
+        user,
+        message
+    ):
+
+        print("Receiver answer")
