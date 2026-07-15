62c8f1911229c92599a1214062f0a03f610c7ca6
947770187a <947770187a@gmail.com>
2026-07-15 16:39:57 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,1 +1,65 @@
+import gspread
+from google.oauth2.service_account import Credentials
 
+from config import (
+    GOOGLE_CREDENTIALS_FILE,
+    GOOGLE_SHEET_NAME,
+    SHEET_USERS,
+    SHEET_QUESTIONS,
+    SHEET_SCHEDULE,
+    SHEET_SESSIONS,
+    SHEET_ANSWERS,
+    SHEET_SETTINGS,
+    SHEET_TEMPLATES,
+    SHEET_LOG
+)
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+class GoogleSheets:
+
+    def __init__(self):
+        creds = Credentials.from_service_account_file(
+            GOOGLE_CREDENTIALS_FILE,
+            scopes=SCOPES
+        )
+
+        self.client = gspread.authorize(creds)
+        self.book = self.client.open(GOOGLE_SHEET_NAME)
+
+        self.users = self.book.worksheet(SHEET_USERS)
+        self.questions = self.book.worksheet(SHEET_QUESTIONS)
+        self.schedule = self.book.worksheet(SHEET_SCHEDULE)
+        self.sessions = self.book.worksheet(SHEET_SESSIONS)
+        self.answers = self.book.worksheet(SHEET_ANSWERS)
+        self.settings = self.book.worksheet(SHEET_SETTINGS)
+        self.templates = self.book.worksheet(SHEET_TEMPLATES)
+        self.log = self.book.worksheet(SHEET_LOG)
+
+    def test_connection(self):
+
+        print("=" * 50)
+        print("GOOGLE SHEETS CONNECTED")
+        print("=" * 50)
+
+        print(f"Users: {self.users.row_count}")
+        print(f"Questions: {self.questions.row_count}")
+        print(f"Schedule: {self.schedule.row_count}")
+
+        print("=" * 50)
+
+    def get_users(self):
+
+        return self.users.get_all_records()
+
+    def get_questions(self):
+
+        return self.questions.get_all_records()
+
+    def get_schedule(self):
+
+        return self.schedule.get_all_records()
