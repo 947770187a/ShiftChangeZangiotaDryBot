76edbd413ce3b4947f92cc87b0d22f04baa66dd8
947770187a <947770187a@gmail.com>
2026-07-18 18:27:20 +0500

    Update registration_manager.py

--- registration_manager.py
+++ registration_manager.py
@@ -21,3 +21,25 @@ class RegistrationManager:
                 "Введите ваши Фамилию Имя Отчество."
             )
         )
+
+        async def process_registration(
+        self,
+        telegram_id,
+        full_name
+    ):
+
+        user_id = str(uuid.uuid4())
+
+        self.sheets.add_user(
+            user_id=user_id,
+            full_name=full_name,
+            telegram_id=telegram_id
+        )
+
+        await self.bot.send_message(
+            chat_id=telegram_id,
+            text=(
+                "✅ Регистрация успешно завершена.\n\n"
+                "Теперь вы можете пользоваться ботом."
+            )
+        )
