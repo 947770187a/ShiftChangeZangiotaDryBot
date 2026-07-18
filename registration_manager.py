85e0664be4e6dce2ddc6e06fe42cc6547e1bf3f2
947770187a <947770187a@gmail.com>
2026-07-18 18:44:07 +0500

    Update registration_manager.py

--- registration_manager.py
+++ registration_manager.py
@@ -27,6 +27,9 @@ class RegistrationManager:
         )
 
     async def process_registration(
+        print(">>> process_registration")
+        print(telegram_id)
+        print(full_name)
         self,
         telegram_id,
         full_name
@@ -35,6 +38,7 @@ class RegistrationManager:
         user_id = str(uuid.uuid4())
 
         self.sheets.add_user(
+        print(">>> add_user finished")
             user_id=user_id,
             full_name=full_name,
             telegram_id=telegram_id
